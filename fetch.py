dad1d0ece00a8bbff32588580d673a96d075bed5
Jesse Schwartzentruber <truber@mozilla.com>
2017-06-16 17:05:34 -0400

    Add support for specifying an arbitrary namespace for --build
    If using Fetcher() class, you can leave branch=None,asan=False,debug=False and they will be inferred.

--- fetch.py
+++ fetch.py
@@ -26,6 +26,9 @@ import requests
 log = logging.getLogger('fuzzfetch')  # pylint: disable=invalid-name
 
 
+BUG_URL = 'https://github.com/MozillaSecurity/fuzzfetch/issues/'
+
+
 def _get_url(url):
     try:
         data = requests.get(url, stream=True)
@@ -71,6 +74,29 @@ class Fetcher(object):
 
         self.build_info = _get_url(self.artifact_url('json')).json()
         self.moz_info = _get_url(self.artifact_url('mozinfo.json')).json()
+
+        # if the build string contains the platform, assume it is a TaskCluster namespace
+        if self.moz_info["platform_guess"] in self._build:
+            # try to set args to match the namespace given
+            if self._branch is None:
+                branch = re.search(r'\.mozilla-(?P<branch>[a-z]+[0-9]*)\.', self._build)
+                self._branch = branch.group('branch') if branch is not None else '?'
+            if not self._debug:
+                self._debug = '-debug' in self._build or '-dbg' in self._build
+            if not self._asan:
+                self._asan = '-asan' in self._build
+
+            # '?' is special case used for unknown build types
+            if self._branch != '?' and self._branch not in self._build:
+                raise Exception("'build' and 'branch' arguments do not match. "
+                                "(build={}, branch={})".format(self._build, self._branch))
+            if self._asan and '-asan' not in self._build:
+                raise Exception("'build' is not an asan build, but asan=True given "
+                                "(build={})".format(self._build))
+            if self._debug and not ('-dbg' in self._build or '-debug' in self._build):
+                raise Exception("'build' is not a debug build, but debug=True given "
+                                "(build={})".format(self._build))
+
 
     def _get_pushdate_url(self, build_arg, target_platform):
         """
@@ -165,14 +191,14 @@ class Fetcher(object):
         Requires first generating the task URL based on the specified build type and platform
         """
         # Prepare build type
-        url_base = 'https://index.taskcluster.net/v1/task/gecko.v2.mozilla-{0}'.format(self._branch)
+        url_base = 'https://index.taskcluster.net/v1/task/'
         target_platform = 'macosx64' if sys.platform == 'darwin' else 'linux64'
 
-        if re.match(r'\d{4}-\d{2}-\d{2}', self._build):
+        if re.match(r'\d{4}-\d{2}-\d{2}$', self._build):
             build_arg = self._build.replace('-', '.')
             task_url = self._get_pushdate_url(build_arg, target_platform)
 
-        elif re.match(r'[0-9A-F]{40}', self._build, re.IGNORECASE):
+        elif re.match(r'[0-9A-F]{40}$', self._build, re.IGNORECASE):
             build_arg = self._build.lower()
             task_url = self._get_revision_url(build_arg, target_platform)
 
@@ -183,10 +209,13 @@ class Fetcher(object):
                 '-debug' if self._debug else '-opt'
             )
 
-            task_url = '{0}.{1}.firefox.{2}'.format(url_base, self._build, build_options)
+            task_url = '{}gecko.v2.mozilla-{}.latest.firefox.{}'.format(url_base, self._branch, build_options)
 
         else:
-            raise Exception('Invalid build value specified: %s' % self._build)
+            # try to use build argument directly as a namespace
+            if target_platform not in self._build:
+                log.warning('Cross-platform fetching is not tested. Please report problems to: %s', BUG_URL)
+            task_url = url_base + self._build
 
         return task_url
 
@@ -332,10 +361,18 @@ class Fetcher(object):
             shutil.rmtree(out_tmp)
             os.unlink(dmg_fn)
 
+    def get_auto_name(self):
+        if self.moz_info["platform_guess"] in self._build:
+            options = self._build.split(self.moz_info["platform_guess"], 1)[1]
+        else:
+            options = ('-asan' if self._asan else '') + ('-debug' if self._debug else '-opt')
+
+        return '{}-{}{}'.format('m-%s' % self._branch[0], self.rank, options)
+
     @staticmethod
     def parse_args():
         parser = argparse.ArgumentParser()
-        parser.set_defaults(target='firefox', branch='central', build='latest', tests=None)
+        parser.set_defaults(target='firefox', build='latest', tests=None) # branch default is set after parsing
 
         target_group = parser.add_argument_group('Target')
         target_group.add_argument('--target', choices=['firefox', 'js'], dest='target',
@@ -381,8 +418,20 @@ class Fetcher(object):
 
         args = parser.parse_args()
 
-        if args.branch == 'esr52' and args.asan and args.debug:
-            parser.error('ESR does not support ASAN debug builds!')
+        if re.match(r'(\d{4}-\d{2}-\d{2}|[0-9A-Fa-f]{40}|latest)$', args.build) is None:
+            # this is a custom build
+            # ensure conflicting options are not set
+            if args.branch is not None:
+                parser.error('Cannot specify --build namespace and branch argument: {}'.format(args.branch))
+            if args.debug:
+                parser.error('Cannot specify --build namespace and --debug')
+            if args.asan:
+                parser.error('Cannot specify --build namespace and --asan')
+
+        # do this default manually so we can error if combined with --build namespace
+        #parser.set_defaults(branch='central')
+        elif args.branch is None:
+            args.branch = 'central'
 
         return args
 
@@ -400,12 +449,7 @@ class Fetcher(object):
         obj = cls(args.target, args.branch, args.build, args.asan, args.debug, args.tests, args.symbols)
 
         if args.name is None:
-            args.name = '{0}-{1}{2}{3}'.format(
-                'm-%s' % args.branch[0],
-                obj.rank,
-                '-asan' if args.asan else '',
-                '-debug' if args.debug else '-opt'
-            )
+            args.name = obj.get_auto_name()
 
         final_dir = os.path.normpath(os.path.abspath(os.path.join(args.out, args.name)))
         if os.path.exists(final_dir):
