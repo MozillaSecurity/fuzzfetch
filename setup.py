45fe966f94ca2b1c0a6b9d5d47fb30a7bac957ff
Jesse Schwartzentruber <truber@mozilla.com>
2017-08-31 09:04:01 -0400

    CI config updates.

--- setup.py
+++ setup.py
@@ -2,24 +2,15 @@
 "setuptools install script"
 from setuptools import setup
 
-
-def main():
-    "setuptools main"
-    with open('requirements.txt') as req_fp:
-        requires = req_fp.read().strip().splitlines()
-    setup(
-        name='fuzzfetch',
-        version='0.5.2',
-        packages=['fuzzfetch'],
-        install_requires=requires,
-        url='https://github.com/MozillaSecurity/fuzzfetch',
-        license='MPL 2.0',
-        author='Jesse Schwartzentruber, Jason Kratzer',
-        description='Downloader for firefox/jsshell builds',
-        entry_points={"console_scripts": ["fuzzfetch = fuzzfetch:Fetcher.main"]},
-        package_dir={"": "src"}
-    )
-
-
-if __name__ == "__main__":
-    main()
+setup(
+    name='fuzzfetch',
+    version='0.5.2',
+    packages=['fuzzfetch'],
+    license='MPL 2.0',
+    url='https://github.com/MozillaSecurity/fuzzfetch',
+    install_requires=open('requirements.txt').read().strip().splitlines(),
+    author='Jesse Schwartzentruber, Jason Kratzer',
+    description='Downloader for firefox/jsshell builds',
+    entry_points={"console_scripts": ["fuzzfetch = fuzzfetch:Fetcher.main"]},
+    package_dir={"": "src"}
+)
