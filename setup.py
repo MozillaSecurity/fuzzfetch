2c2b28f7b5ebd60094e6ba68625c35feb37ff56c
Jesse Schwartzentruber <truber@mozilla.com>
2017-09-19 12:09:29 -0400

    Fix indentation of setup.py for flake8

--- setup.py
+++ setup.py
@@ -9,29 +9,30 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup(classifiers=[
-              "Intended Audience :: Developers",
-              "Topic :: Software Development :: Testing",
-              "Topic :: Security",
-              "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
-              "Programming Language :: Python :: 2",
-              "Programming Language :: Python :: 2.7",
-              "Programming Language :: Python :: 3",
-              "Programming Language :: Python :: 3.4",
-              "Programming Language :: Python :: 3.5",
-              "Programming Language :: Python :: 3.6"
-          ],
-          description='Downloader for firefox/jsshell builds',
-          entry_points={
-              "console_scripts": ["fuzzfetch = fuzzfetch:Fetcher.main"]
-          },
-          install_requires=open('requirements.txt').read().strip().splitlines(),
-          keywords="fuzz fuzzing security test testing",
-          license="MPL 2.0",
-          maintainer="Mozilla Fuzzing Team",
-          maintainer_email="fuzzing@mozilla.com",
-          name="fuzzfetch",
-          package_dir={"": "src"},
-          packages=["fuzzfetch"],
-          url="https://github.com/MozillaSecurity/fuzzfetch",
-          version="0.5.4")
+    setup(
+        classifiers=[
+            "Intended Audience :: Developers",
+            "Topic :: Software Development :: Testing",
+            "Topic :: Security",
+            "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6"
+        ],
+        description='Downloader for firefox/jsshell builds',
+        entry_points={
+            "console_scripts": ["fuzzfetch = fuzzfetch:Fetcher.main"]
+        },
+        install_requires=open('requirements.txt').read().strip().splitlines(),
+        keywords="fuzz fuzzing security test testing",
+        license="MPL 2.0",
+        maintainer="Mozilla Fuzzing Team",
+        maintainer_email="fuzzing@mozilla.com",
+        name="fuzzfetch",
+        package_dir={"": "src"},
+        packages=["fuzzfetch"],
+        url="https://github.com/MozillaSecurity/fuzzfetch",
+        version="0.5.4")
