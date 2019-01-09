fa09beaefef8b5e0a5e8991af2f2f067b7cd428e
Jesse Schwartzentruber <truber@mozilla.com>
2019-01-09 16:45:18 -0500

    Update version to 0.8.0

--- setup.py
+++ setup.py
@@ -39,4 +39,4 @@ if __name__ == "__main__":
         package_dir={"": "src"},
         packages=["fuzzfetch"],
         url="https://github.com/MozillaSecurity/fuzzfetch",
-        version="0.7.0")
+        version="0.8.0")
