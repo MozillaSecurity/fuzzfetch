fee480200a1946eed5d920bd77f814e2b1066621
Jesse Schwartzentruber <truber@mozilla.com>
2019-12-04 11:36:12 -0500

    Bump version to 0.9.3

--- setup.py
+++ setup.py
@@ -41,4 +41,4 @@ if __name__ == "__main__":
         package_dir={"": "src"},
         packages=["fuzzfetch"],
         url="https://github.com/MozillaSecurity/fuzzfetch",
-        version="0.9.2")
+        version="0.9.3")
