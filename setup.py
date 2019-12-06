4f5806eb05be7aeeb3c5bac65fd911dfac5d69e7
Jesse Schwartzentruber <truber@mozilla.com>
2019-12-06 10:00:22 -0500

    Packaging fix for 2.7.

--- setup.py
+++ setup.py
@@ -10,6 +10,9 @@ from setuptools import setup
 
 if __name__ == "__main__":
     setup(
+        # package_dir needed here for 2.7 only.
+        # see https://github.com/pypa/setuptools/issues/1136
+        package_dir={"": "src"},
+        setup_requires=["setuptools_scm"],
         use_scm_version=True,
-        setup_requires=['setuptools_scm'],
     )
