35f6846c31c7ddd014e626e41a45c83970152ee4
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-22 21:07:36 +0000

    Adding tarfile member sanitization to extractall()

--- src/fuzzfetch/extract.py
+++ src/fuzzfetch/extract.py
@@ -78,7 +78,26 @@ def extract_tar(tar_fn: PathArg, mode: str = "", path: PathArg = ".") -> None:
                 elif member.path != "firefox":
                     # Ignore top-level build directory
                     members.append(member)
-            tar.extractall(members=members, path=path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, members=members, path=path)
     finally:
         if P7Z_PATH:
             os.unlink(p7z_fn)
