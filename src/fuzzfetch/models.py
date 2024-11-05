b3fb7936418ff16ee3a6c36026c4ee071c55734a
pyoor <pyoor@users.noreply.github.com>
2024-11-05 11:45:42 -0500

    docs: set module docstring below license

--- src/fuzzfetch/models.py
+++ src/fuzzfetch/models.py
@@ -1,8 +1,7 @@
-"""fuzzfetch internal models"""
-
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
+"""Fuzzfetch internal models"""
 
 import itertools
 import platform as std_platform
