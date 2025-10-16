MozillaSecurity/fuzzfetch

commit bcd1ddd42864f329996d0dcf8311e52424ad0034

fix: Check for zst test artifacts

src/fuzzfetch/core.py
@@ -431,9 +431,11 @@ class Fetcher:
             resolve_url(self.artifact_url("mozsearch-distinclude.map"))
 
         for target in targets_remaining:
-            try:
-                resolve_url(self.artifact_url(f"{target}.tests.tar.gz"))
-            except FetcherException:  # noqa: PERF203
+            for ext in ("zst", "gz"):
+                with suppress(FetcherException):
+                    resolve_url(self.artifact_url(f"{target}.tests.tar.{ext}"))
+                    break
+            else:
                 resolve_url(self.artifact_url(f"{target}.tests.zip"))
 
     def extract_build(self, path: PathArg) -> None:
@@ -492,10 +494,12 @@ class Fetcher:
 
         if "gtest" in targets_remaining:
             targets_remaining.remove("gtest")
-            try:
-                self.extract_tar(self.artifact_url("gtest.tests.tar.gz"), path)
-            except FetcherException:
-                self.extract_zip(self.artifact_url("gtest.tests.zip"), path)
+            for ext in ("zst", "gz"):
+                with suppress(FetcherException):
+                    self.extract_tar(self.artifact_url(f"gtest.tests.tar.{ext}"))
+                    break
+            else:
+                self.extract_zip(self.artifact_url("gtest.tests.zip"))
             if self._platform.system == "Windows":
                 libxul = "xul.dll"
             elif self._platform.system == "Linux":
@@ -546,10 +550,12 @@ class Fetcher:
 
         # any still remaining targets are assumed to be test artifacts
         for target in targets_remaining:
-            try:
-                self.extract_tar(self.artifact_url(f"{target}.tests.tar.gz"), path=path)
-            except FetcherException:  # noqa: PERF203
-                self.extract_zip(self.artifact_url(f"{target}.tests.zip"), path=path)
+            for ext in ("zst", "gz"):
+                with suppress(FetcherException):
+                    self.extract_tar(self.artifact_url(f"{target}.tests.tar.{ext}"))
+                    break
+            else:
+                self.extract_zip(self.artifact_url(f"{target}.tests.zip"))
 
         # used by Pernosco to locate source ('\n' is expected)
         (path / "taskcluster-build-task").write_bytes(f"{self.task_id}\n".encode())

commit 9c0b6099bfa1e311655e9b99678785f542bf1128

fix: dropped path argument to extract methods
regressed by bcd1ddd42864f329996d0dcf8311e52424ad0034

src/fuzzfetch/core.py
@@ -491,10 +491,10 @@ class Fetcher:
             targets_remaining.remove("gtest")
             for ext in ("zst", "gz"):
                 with suppress(FetcherException):
-                    self.extract_tar(self.artifact_url(f"gtest.tests.tar.{ext}"))
+                    self.extract_tar(self.artifact_url(f"gtest.tests.tar.{ext}"), path)
                     break
             else:
-                self.extract_zip(self.artifact_url("gtest.tests.zip"))
+                self.extract_zip(self.artifact_url("gtest.tests.zip"), path)
             if self._platform.system == "Windows":
                 libxul = "xul.dll"
             elif self._platform.system == "Linux":
@@ -547,10 +547,12 @@ class Fetcher:
         for target in targets_remaining:
             for ext in ("zst", "gz"):
                 with suppress(FetcherException):
-                    self.extract_tar(self.artifact_url(f"{target}.tests.tar.{ext}"))
+                    self.extract_tar(
+                        self.artifact_url(f"{target}.tests.tar.{ext}"), path
+                    )
                     break
             else:
-                self.extract_zip(self.artifact_url(f"{target}.tests.zip"))
+                self.extract_zip(self.artifact_url(f"{target}.tests.zip"), path)
 
         # used by Pernosco to locate source ('\n' is expected)
         (path / "taskcluster-build-task").write_bytes(f"{self.task_id}\n".encode())