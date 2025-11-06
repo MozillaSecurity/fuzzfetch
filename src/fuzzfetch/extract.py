71edb37312479e7bdc3b8cb6a6535f2f32df96e1
Jesse Schwartzentruber <truber@mozilla.com>
2025-11-06 16:53:23 -0500

    fix: broken type annotation in 3.9

--- src/fuzzfetch/extract.py
+++ src/fuzzfetch/extract.py
@@ -3,6 +3,8 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 """Code for extracting archives"""
 
+from __future__ import annotations
+
 import os
 from logging import getLogger
 from os.path import abspath, commonpath
